1ec7565a8fec35ee9ef2acf3af9d7404412f451d
Lisa <mclisa00@gmail.com>
2023-01-26 14:58:07 +0000

    Completed lessons

---
+++ week_01/day_4/modules_packages/runner.py
@@ -0,0 +1,8 @@
+# import calculator as calc
+import modules.calculator as calc
+from another_module.calculator import multiply
+
+# from calculator import *
+# import calculator
+
+print(calc.add(10, 20))
