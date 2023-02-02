767837d1cee2bd7fb1d8bc7df56df98d84e0e67e
Lisa <mclisa00@gmail.com>
2023-02-02 15:58:41 +0000

    Messing around with package names

--- week_02/day_3/coffee_shop/run_tests.py
+++ week_02/day_3/coffee_shop/run_tests.py
@@ -1,8 +1,10 @@
 import unittest
+
 from tests.coffee_shop_test import TestCoffeeShop
 from tests.drinks_test import TestDrink
 from tests.customer_test import TestCustomer
 from tests.food_test import TestFood
+
 
 if __name__ == "__main__":
     unittest.main()
