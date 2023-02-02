44d8f4ed6f56f49197c8c3db13bf68f43e82b590
Lisa <mclisa00@gmail.com>
2023-02-02 11:44:53 +0000

    Finished lesson on TDD

--- week_02/day_4/tdd/tdd_start_code/src/compare.py
+++ week_02/day_4/tdd/tdd_start_code/src/compare.py
@@ -1,7 +1,7 @@
-def compare(num1, num2):
-    if num1 < num2:
-        return f"{num1} is less than {num2}"
-    elif num1 == num2:
+def compare(number_1, number_2):
+    if number_1 < number_2:
+        return f"{number_1} is less than {number_2}"
+    elif number_1 == number_2:
         return "The numbers are equal"
     else:
-        return f"{num1} is greater than {num2}"
+        return f"{number_1} is greater than {number_2}"
