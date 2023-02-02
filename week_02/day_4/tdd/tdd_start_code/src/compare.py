7e6f7aa67ad7ad6e9abef6e01fe5e7dc49e36ea2
Lisa <mclisa00@gmail.com>
2023-02-02 11:25:15 +0000

    Added 3 tests for compare()

--- week_02/day_4/tdd/tdd_start_code/src/compare.py
+++ week_02/day_4/tdd/tdd_start_code/src/compare.py
@@ -1,2 +1,7 @@
-def compare():
-    pass
+def compare(num1, num2):
+    if num1 < num2:
+        return f"{num1} is less than {num2}"
+    elif num1 == num2:
+        return "The numbers are equal"
+    else:
+        return f"{num1} is greater than {num2}"
--- week_02/day_4/tdd/tdd_start_code/tests/compare_test.py
+++ week_02/day_4/tdd/tdd_start_code/tests/compare_test.py
@@ -2,7 +2,13 @@ import unittest
 
 from src.compare import compare
 
-class TestCompare(unittest.TestCase):
 
-    def test_compare_3_1_returns_3_is_greater_than_1(self):
+class TestCompare(unittest.TestCase):
+    def test_compare__3_1_returns_3_is_greater_than_1(self):
         self.assertEqual("3 is greater than 1", compare(3, 1))
+
+    def test_compare__5_10_returns_5_is_less_than_10(self):
+        self.assertEqual("5 is less than 10", compare(5, 10))
+
+    def test_compare__7_7_return_the_numbers_are_equal(self):
+        self.assertEqual("The numbers are equal", compare(7, 7))
