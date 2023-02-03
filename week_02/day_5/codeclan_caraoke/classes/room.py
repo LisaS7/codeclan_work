2dce4a1698e0212751577316fc9f522696e39734
Lisa <mclisa00@gmail.com>
2023-02-03 11:27:13 +0000

    Added tests to Room for capacity, entry fee, and adding songs

--- week_02/day_5/codeclan_caraoke/classes/room.py
+++ week_02/day_5/codeclan_caraoke/classes/room.py
@@ -1,11 +1,23 @@
 class Room:
-    def __init__(self, name):
+    def __init__(self, name, capacity, entry_fee):
         self.name = name
         self.guests = []
         self.songs = []
+        self.capacity = capacity
+        self.entry_fee = entry_fee
 
     def check_in(self, guest):
-        self.guests.append(guest)
+        if self.room_has_space():
+            self.guests.append(guest)
 
     def check_out(self, guest):
         self.guests.remove(guest)
+
+    def number_of_guests(self):
+        return len(self.guests)
+
+    def room_has_space(self):
+        return self.number_of_guests() < self.capacity
+
+    def add_song(self, song):
+        self.songs.append(song)
--- week_02/day_5/codeclan_caraoke/tests/room_test.py
+++ week_02/day_5/codeclan_caraoke/tests/room_test.py
@@ -6,7 +6,7 @@ from classes.song import Song
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        self.room = Room("Ash")
+        self.room = Room("Ash", 5, 10)
         self.song1 = Song("Before I Forget", "Slipknot")
         self.song2 = Song("The Unforgiven", "Metallica")
         self.guest1 = Guest("Corey Taylor", 200, self.song1)
@@ -21,12 +21,40 @@ class TestRoom(unittest.TestCase):
     def test_has_empty_song_list(self):
         self.assertEqual([], self.room.songs)
 
-    def test_check_in_guest(self):
+    def test_has_entry_fee(self):
+        self.assertEqual(10, self.room.entry_fee)
+
+    def test_check_in_guest__success(self):
         self.room.check_in(self.guest1)
-        self.assertEqual(self.guest1, self.room.guests[0])
+        self.assertIn(self.guest1, self.room.guests)
+
+    def test_check_in_guest__fail(self):
+        for _ in range(5):
+            self.room.check_in(self.guest1)
+        self.room.check_in(self.guest2)
+        self.assertNotIn(self.guest2, self.room.guests)
 
     def test_check_out_guest(self):
         self.room.check_in(self.guest1)
         self.room.check_in(self.guest2)
         self.room.check_out(self.guest1)
-        self.assertEqual(self.guest2, self.room.guests[0])
+        self.assertEqual(1, self.room.number_of_guests())
+
+    def test_number_of_guests(self):
+        self.room.check_in(self.guest1)
+        self.room.check_in(self.guest2)
+        self.assertEqual(2, self.room.number_of_guests())
+
+    def test_room_has_space__true(self):
+        self.room.check_in(self.guest1)
+        self.room.check_in(self.guest2)
+        self.assertEqual(True, self.room.room_has_space())
+
+    def test_room_has_space__false(self):
+        for _ in range(5):
+            self.room.check_in(self.guest1)
+        self.assertEqual(False, self.room.room_has_space())
+
+    def test_add_song(self):
+        self.room.add_song(self.song1)
+        self.assertIn(self.song1, self.room.songs)
