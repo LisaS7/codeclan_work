51b403de76c665268f86d5755ce49fb16621ff08
Lisa <mclisa00@gmail.com>
2023-02-03 14:19:17 +0000

    Changed Song to Dataclass

--- week_02/day_5/codeclan_caraoke/classes/song.py
+++ week_02/day_5/codeclan_caraoke/classes/song.py
@@ -1,4 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Song:
-    def __init__(self, title, artist):
-        self.title = title
-        self.artist = artist
+    title: str
+    artist: str
