31257f7698873c40d621c805993efb35551ff340
Lisa <mclisa00@gmail.com>
2023-02-03 10:26:23 +0000

    Song tests pass

--- week_02/day_5/codeclan_caraoke/tests/song_test.py
+++ week_02/day_5/codeclan_caraoke/tests/song_test.py
@@ -1,5 +1,13 @@
 import unittest
+from classes.song import Song
 
 
 class TestSong(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.song = Song("American Pie", "Don Mclean")
+
+    def test_has_title(self):
+        self.assertEqual("American Pie", self.song.title)
+
+    def test_has_artist(self):
+        self.assertEqual("Don Mclean", self.song.artist)
