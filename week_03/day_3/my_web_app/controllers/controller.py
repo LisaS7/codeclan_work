49b2d46d0eac5229ad4bed0e415a6f9dd3a3b841
Lisa <mclisa00@gmail.com>
2023-02-08 11:43:31 +0000

    Flask lesson done

--- week_03/day_3/my_web_app/controllers/controller.py
+++ week_03/day_3/my_web_app/controllers/controller.py
@@ -4,3 +4,13 @@ from app import app
 @app.route("/")
 def index():
     return "Hello Alec!"
+
+
+@app.route("/greet/<name>")
+def greet(name):
+    return f"Hello {name}"
+
+
+@app.route("/news")
+def news():
+    return "This is the news page"
