ef67fd60aee1ae8a043b88fdc57b15128197b679
Lisa <mclisa00@gmail.com>
2023-02-08 14:58:34 +0000

    Flask lab styling and single order page

--- week_03/day_3/flask_lab/controllers/controller.py
+++ week_03/day_3/flask_lab/controllers/controller.py
@@ -6,3 +6,10 @@ from models.orders import orders
 @app.route("/orders")
 def orders_index():
     return render_template("index.html", orders=orders)
+
+
+@app.route("/order/<index>")
+def order(index):
+    index = int(index)
+    order = orders[index]
+    return render_template("order.html", order=order)
