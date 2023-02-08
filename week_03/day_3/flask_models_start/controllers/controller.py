9aa2ab3f9713b2e8c8270f3b8e670175751fee03
Lisa <mclisa00@gmail.com>
2023-02-08 12:26:46 +0000

    Models/Jinja lesson

--- week_03/day_3/flask_models_start/controllers/controller.py
+++ week_03/day_3/flask_models_start/controllers/controller.py
@@ -1,5 +1,13 @@
 from app import app
+from flask import render_template
+from models.todo_list import tasks
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Hello World!"
+
+
+@app.route("/tasks")
+def tasks_index():
+    return render_template("index.html", title="Home", tasks=tasks)
