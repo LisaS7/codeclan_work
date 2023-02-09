d311da4dcfcc8da7d7718a8b2aafd60802fe463a
Lisa <mclisa00@gmail.com>
2023-02-09 13:57:08 +0000

    Updated class for Event

---
+++ week_03/day_4/lab/models/event.py
@@ -0,0 +1,7 @@
+class Event:
+    def __init__(self, date, title, guests, room, description):
+        self.date = date
+        self.title = title
+        self.guests = guests
+        self.room = room
+        self.description = description
--- week_03/day_4/lab/models/todo_list.py
+++ week_03/day_4/lab/models/todo_list.py
@@ -1,10 +1,14 @@
-from models.task import *
+from models.event import *
+from datetime import datetime
+
+event1 = Event(
+    datetime(2023, 5, 10), "Birthday party", 28, "Room1", "Frank's 70th birthday"
+)
+event2 = Event(
+    datetime(2023, 8, 23), "PyCon", 150, "Conference Room", "Python Conference"
+)
+events = [event1, event2]
 
 
-task1 = Task("Buy groceries", "Milk, Cheese, Pizza, Fruit", False)
-task2 = Task("Learn Python", "Learn an awesome new programming language", True)
-tasks = [task1, task2]
-
-
-def add_new_task(task):
-    tasks.append(task)
+def add_new_task(event):
+    events.append(event)
