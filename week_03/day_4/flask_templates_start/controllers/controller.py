474afe4e901fd4fc576bf81ed1325217e3752421
Lisa <mclisa00@gmail.com>
2023-02-09 12:07:27 +0000

    Finished post controller

--- week_03/day_4/flask_templates_start/controllers/controller.py
+++ week_03/day_4/flask_templates_start/controllers/controller.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app import app
-from models.todo_list import tasks
+from models.todo_list import tasks, Task, add_new_task
 
 
 @app.route("/tasks")
@@ -10,4 +10,13 @@ def index():
 
 @app.route("/tasks/new")
 def new_task():
-    return render_template("new.html")
+    return render_template("new.html", title="New Task")
+
+
+@app.route("/tasks", methods=["POST"])
+def add_task():
+    task_title = request.form["title"]
+    task_desc = request.form["description"]
+    new_task = Task(task_title, task_desc, False)
+    add_new_task(new_task)
+    return redirect("/tasks")
