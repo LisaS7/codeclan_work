76d671c109620ed91fe648bf6313e964a3647bbd
Lisa <mclisa00@gmail.com>
2023-02-10 11:12:26 +0000

    Added route for list books

--- week_03/day_5/homework/library/controllers/controller.py
+++ week_03/day_5/homework/library/controllers/controller.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import render_template
+from models.library import all_books, add_book, remove_book
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/all-books")
+def list_books():
+    return render_template("all_books.html", books=all_books)
--- week_03/day_5/homework/library/models/library.py
+++ week_03/day_5/homework/library/models/library.py
@@ -1,6 +1,11 @@
 from models.book import Book
 
-all_books = []
+book1 = Book("Clean Code", "Robert C Martin", "Programming")
+book2 = Book("The Pragmatic Programmer", "Andrew Hunt, David Thomas", "Programming")
+book3 = Book("V For Vendetta", "Alan Moore", "Graphic Novel")
+book4 = Book("Batman: The Long Halloween", "Jeph Loeb", "Graphic Novel")
+
+all_books = [book1, book2, book3, book4]
 
 
 def add_book(book):
