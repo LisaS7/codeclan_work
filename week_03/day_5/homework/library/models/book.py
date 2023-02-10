5d2ef367b2f78192c7d143e34b8cf46e01023dea
Lisa <mclisa00@gmail.com>
2023-02-10 11:44:57 +0000

    Added images

--- week_03/day_5/homework/library/models/book.py
+++ week_03/day_5/homework/library/models/book.py
@@ -1,5 +1,6 @@
 class Book:
-    def __init__(self, title, author, genre):
+    def __init__(self, title, author, genre, image):
         self.title = title
         self.author = author
         self.genre = genre
+        self.image = image
--- week_03/day_5/homework/library/models/library.py
+++ week_03/day_5/homework/library/models/library.py
@@ -1,9 +1,19 @@
 from models.book import Book
 
-book1 = Book("Clean Code", "Robert C Martin", "Programming")
-book2 = Book("The Pragmatic Programmer", "Andrew Hunt, David Thomas", "Programming")
-book3 = Book("V For Vendetta", "Alan Moore", "Graphic Novel")
-book4 = Book("Batman: The Long Halloween", "Jeph Loeb", "Graphic Novel")
+book1 = Book("Clean Code", "Robert C Martin", "Programming", "clean_code.jpeg")
+book2 = Book(
+    "The Pragmatic Programmer",
+    "Andrew Hunt, David Thomas",
+    "Programming",
+    "pragmatic-programmer.jpeg",
+)
+book3 = Book("V For Vendetta", "Alan Moore", "Graphic Novel", "v_for_vendetta.jpeg")
+book4 = Book(
+    "Batman: The Long Halloween",
+    "Jeph Loeb",
+    "Graphic Novel",
+    "batman_the_long_halloween.jpeg",
+)
 
 all_books = [book1, book2, book3, book4]
 
