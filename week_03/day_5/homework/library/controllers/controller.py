e32542033f8d9a5ca1716fe9fcfae53dc4c4a4d0
Lisa <mclisa00@gmail.com>
2023-02-10 11:53:46 +0000

    Added delete route

--- week_03/day_5/homework/library/controllers/controller.py
+++ week_03/day_5/homework/library/controllers/controller.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect
 from models.library import all_books, add_book, remove_book
 
 
@@ -16,4 +16,11 @@ def list_books():
 @app.route("/book/<index>")
 def single_book(index):
     index = int(index)
-    return render_template("single_book.html", book=all_books[index])
+    return render_template("single_book.html", book=all_books[index], index=index)
+
+
+@app.route("/book/delete/<index>")
+def delete_book(index):
+    book = all_books[int(index)]
+    remove_book(book)
+    return redirect("/all-books")
