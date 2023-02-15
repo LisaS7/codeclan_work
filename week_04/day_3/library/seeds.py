02637ffd6300d65675e4820d4f002120d64a9ea8
Lisa <mclisa00@gmail.com>
2023-02-15 16:30:12 +0000

    Set up models and db

---
+++ week_04/day_3/library/seeds.py
@@ -0,0 +1,41 @@
+from models.book import Book
+
+# import repositories
+
+# delete all
+
+book1 = Book(
+    "Clean Code",
+    "Robert C Martin",
+    "Programming",
+    "Pearson Education",
+    464,
+    "clean_code.jpeg",
+)
+book2 = Book(
+    "The Pragmatic Programmer",
+    "Andrew Hunt, David Thomas",
+    "Programming",
+    "Addison-Wesley",
+    352,
+    "pragmatic-programmer.jpeg",
+)
+book3 = Book(
+    "V For Vendetta",
+    "Alan Moore",
+    "Graphic Novel",
+    "Quality Communications",
+    296,
+    "v_for_vendetta.jpeg",
+)
+book4 = Book(
+    "Batman: The Long Halloween",
+    "Jeph Loeb",
+    "Graphic Novel",
+    "DC Comics",
+    384,
+    "batman_the_long_halloween.jpeg",
+)
+
+
+# save books
