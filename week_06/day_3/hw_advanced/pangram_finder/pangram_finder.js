dc0620283436553d6babcc33a506df68184baa94
Lisa <mclisa00@gmail.com>
2023-03-01 11:18:19 +0000

    Complete

--- week_06/day_3/hw_advanced/pangram_finder/pangram_finder.js
+++ week_06/day_3/hw_advanced/pangram_finder/pangram_finder.js
@@ -1,9 +1,11 @@
-const PangramFinder = function (phrase) {
-  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-}
-
-PangramFinder.prototype.isPangram = function () {
-
+class PangramFinder {
+  constructor(phrase) {
+    this.alphabet = "qwertyuiopasdfghjklzxcvbnm".split("");
+    this.phrase = phrase.split("").map((letter) => letter.toLowerCase());
+  }
+  isPangram() {
+    return this.alphabet.every((letter) => this.phrase.includes(letter));
+  }
 }
 
 module.exports = PangramFinder;
