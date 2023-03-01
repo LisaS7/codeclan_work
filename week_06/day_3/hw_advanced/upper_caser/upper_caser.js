c3b979027fdb3737955d42b0acc5c3be70d3a080
Lisa <mclisa00@gmail.com>
2023-03-01 10:55:01 +0000

    Complete

--- week_06/day_3/hw_advanced/upper_caser/upper_caser.js
+++ week_06/day_3/hw_advanced/upper_caser/upper_caser.js
@@ -1,8 +1,10 @@
-const UpperCaser = function (words) {
-}
-
-UpperCaser.prototype.toUpperCase = function () {
-
+class UpperCaser {
+  constructor(words) {
+    this.words = words;
+  }
+  toUpperCase() {
+    return this.words.map((word) => word.toUpperCase());
+  }
 }
 
 module.exports = UpperCaser;
