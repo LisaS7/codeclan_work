08e22a9493348e0b6a30ba50ebca74894925bd42
Lisa <mclisa00@gmail.com>
2023-03-01 11:40:34 +0000

    Completed

--- week_06/day_3/hw_advanced/anagram_finder/anagram_finder.js
+++ week_06/day_3/hw_advanced/anagram_finder/anagram_finder.js
@@ -1,9 +1,18 @@
-const AnagramFinder = function (word) {
-
-}
-
-AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+class AnagramFinder {
+  constructor(word) {
+    this.word = word;
+  }
+  sortString(word) {
+    return word.toLowerCase().split("").sort().join("");
+  }
+  findAnagrams(otherWords) {
+    const sortedThisWord = this.sortString(this.word);
+    return otherWords.filter(
+      (word) =>
+        this.sortString(word.toLowerCase()) === sortedThisWord &&
+        this.word !== word
+    );
+  }
 }
 
 module.exports = AnagramFinder;
