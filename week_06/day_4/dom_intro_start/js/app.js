681f2604be85b7d5d5186fe7f90311cdd897b750
Lisa <mclisa00@gmail.com>
2023-03-02 12:05:08 +0000

    DOM lesson complete

--- week_06/day_4/dom_intro_start/js/app.js
+++ week_06/day_4/dom_intro_start/js/app.js
@@ -1,1 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const title = document.querySelector("h1");
+  title.textContent = "JavaScript says 'Hello World!' :)";
 
+  const welcomeParagraph = document.getElementById("welcome-paragraph");
+  //   console.log(welcomeParagraph);
+
+  const redElement = document.querySelector(".red");
+  //   console.log(redElement);
+
+  const redListItem = document.querySelector("li.red");
+  redListItem.textContent = "RED!!!";
+  redListItem.classList.add("bold");
+  //   console.log(redListItem);
+
+  const allRedElements = document.querySelectorAll(".red");
+  //   console.log(allRedElements);
+
+  const newListItem = document.createElement("li");
+  newListItem.textContent = "cheese";
+  newListItem.classList.add("purple");
+  document.querySelector("ul").append(newListItem);
+});
