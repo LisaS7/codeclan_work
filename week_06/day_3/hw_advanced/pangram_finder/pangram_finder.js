4c2f9b1a02ae922dc8a1a8d2e8a16bc61d679fa5
Lisa <mclisa00@gmail.com>
2023-03-02 10:46:59 +0000

    Lesson 1, DOM, start code

--- week_06/day_3/hw_advanced/pangram_finder/pangram_finder.js
+++ week_06/day_3/hw_advanced/pangram_finder/pangram_finder.js
@@ -1,7 +1,7 @@
 class PangramFinder {
   constructor(phrase) {
     this.alphabet = "qwertyuiopasdfghjklzxcvbnm".split("");
-    this.phrase = phrase.split("").map((letter) => letter.toLowerCase());
+    this.phrase = phrase.toLowerCase().split("");
   }
   isPangram() {
     return this.alphabet.every((letter) => this.phrase.includes(letter));
