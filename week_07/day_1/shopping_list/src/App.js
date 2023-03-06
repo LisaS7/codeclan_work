69c9898060787bbfdceba6d360bd005bc360bb51
Lisa <mclisa00@gmail.com>
2023-03-06 14:34:05 +0000

    React afternoon lesson part 1

--- week_07/day_1/shopping_list/src/App.js
+++ week_07/day_1/shopping_list/src/App.js
@@ -1,19 +1,49 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([
+    { name: "Bread", isPurchased: false },
+    { name: "Cheese", isPurchased: true },
+    { name: "Jalapenos", isPurchased: false },
+  ]);
+  const [newItemName, setNewItemName] = useState();
+
+  const itemElements = items.map((item, index) => {
+    return <li key={index}>{item.name}</li>;
+  });
+
+  function saveNewItem(event) {
+    event.preventDefault();
+    const newItem = {
+      name: newItemName,
+      isPurchased: false,
+    };
+    setItems([...items, newItem]);
+    setNewItemName("");
+  }
+
+  function handleInputChange(event) {
+    setNewItemName(event.target.value);
+  }
+
   return (
     <div className="App">
-
       <h1>Shopping List</h1>
       <hr></hr>
 
-      <ul>
-      </ul>
+      <ul>{itemElements}</ul>
 
-      <form>
+      <form onSubmit={saveNewItem}>
+        <label htmlFor="item-name">Add new item:</label>
+        <input
+          id="item-name"
+          type="text"
+          onChange={handleInputChange}
+          value={newItemName}
+        />
+        <button>Submit</button>
       </form>
-
     </div>
   );
 }
