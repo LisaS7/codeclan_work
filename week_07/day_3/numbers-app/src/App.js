edf1f62ed554e556dce95ba4b916848cc1ebbe08
Lisa <mclisa00@gmail.com>
2023-03-08 11:07:04 +0000

    Lesson 1 - useEffect

--- week_07/day_3/numbers-app/src/App.js
+++ week_07/day_3/numbers-app/src/App.js
@@ -2,7 +2,29 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [number, setNumber] = useState(1);
+  const [squaredNumber, setSquaredNumber] = useState(1);
+
+  useEffect(() => {
+    setSquaredNumber(number ** 2);
+  }, [number]);
+
+  function plusNumber() {
+    setNumber(number + 1);
+  }
+
+  function minusNumber(params) {
+    setNumber(number - 1);
+  }
+
+  return (
+    <div className="App">
+      <p>The number is {number}</p>
+      <p>The squared number is {squaredNumber}</p>
+      <button onClick={minusNumber}>-</button>
+      <button onClick={plusNumber}>+</button>
+    </div>
+  );
 }
 
 export default App;
