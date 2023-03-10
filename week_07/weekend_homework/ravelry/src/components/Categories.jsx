47fb273a363e5261d1c45a468014580de943c791
Lisa <mclisa00@gmail.com>
2023-03-10 16:00:54 +0000

    Category button animation

--- week_07/weekend_homework/ravelry/src/components/Categories.jsx
+++ week_07/weekend_homework/ravelry/src/components/Categories.jsx
@@ -1,4 +1,5 @@
 import "./Categories.css";
+import { motion } from "framer-motion";
 
 export default function Categories({ options, setValue }) {
   function changeQuery(query) {
@@ -6,9 +7,18 @@ export default function Categories({ options, setValue }) {
   }
 
   const categoryButtons = options.map((word, index) => (
-    <button key={index} onClick={() => changeQuery(word)}>
+    <motion.button
+      whileHover={{
+        scale: 1.2,
+        boxShadow: "0 0 10px pink",
+        transition: { duration: 0.5 },
+      }}
+      whileTap={{ backgroundColor: "#1c1e27", border: "2px solid pink" }}
+      key={index}
+      onClick={() => changeQuery(word)}
+    >
       {word}
-    </button>
+    </motion.button>
   ));
   return <nav>{categoryButtons}</nav>;
 }
