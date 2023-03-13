e9e36b006ac1637b396f1495a9bc602efac62d21
Lisa <mclisa00@gmail.com>
2023-03-13 15:36:32 +0000

    Refactored tests

--- week_08/day_1/2_react_testing_lab/react_calculator/src/tests/Calculator.test.js
+++ week_08/day_1/2_react_testing_lab/react_calculator/src/tests/Calculator.test.js
@@ -1,94 +1,60 @@
 import React from "react";
 import Calculator from "../containers/Calculator";
+import { buttonMap } from "./button_map";
 import { render, fireEvent } from "@testing-library/react";
-
-function chainClicks(buttons) {
-  for (const button of buttons) {
-    fireEvent.click(button);
-  }
-}
 
 describe("Calculator", () => {
   let container;
-  let button1, button2, button3, button4, button5, button7;
-  let buttonPlus,
-    buttonMinus,
-    buttonMultiply,
-    buttonDivide,
-    buttonEquals,
-    buttonClear;
   let runningTotal;
 
+  function chainClicks(buttons) {
+    for (const button of buttons) {
+      const buttonElement = container.getByTestId(buttonMap[button]);
+      fireEvent.click(buttonElement);
+    }
+  }
   beforeEach(() => {
     container = render(<Calculator />);
-    button1 = container.getByTestId("number1");
-    button2 = container.getByTestId("number2");
-    button3 = container.getByTestId("number3");
-    button4 = container.getByTestId("number4");
-    button5 = container.getByTestId("number5");
-    button7 = container.getByTestId("number7");
-    buttonPlus = container.getByTestId("operator-add");
-    buttonMinus = container.getByTestId("operator-subtract");
-    buttonMultiply = container.getByTestId("operator-multiply");
-    buttonDivide = container.getByTestId("operator-divide");
-    buttonEquals = container.getByTestId("operator-equals");
-    buttonClear = container.getByTestId("clear");
     runningTotal = container.getByTestId("running-total");
   });
 
   it("should change running total on number enter", () => {
-    fireEvent.click(button4);
+    chainClicks([4]);
     expect(runningTotal.textContent).toEqual("4");
   });
 
   it("should add two numbers", () => {
-    chainClicks([button1, buttonPlus, button4, buttonEquals]);
+    chainClicks([1, "+", 4, "="]);
     expect(runningTotal.textContent).toEqual("5");
   });
 
   it("should subtract one number from another", () => {
-    chainClicks([button7, buttonMinus, button4, buttonEquals]);
+    chainClicks([7, "-", 4, "="]);
     expect(runningTotal.textContent).toEqual("3");
   });
 
   it("should multiply two numbers", () => {
-    chainClicks([button3, buttonMultiply, button5, buttonEquals]);
+    chainClicks([3, "*", 5, "="]);
     expect(runningTotal.textContent).toEqual("15");
   });
 
   it("should divide one number by another", () => {
-    chainClicks([button2, button1, buttonDivide, button7, buttonEquals]);
+    chainClicks([2, 1, "/", 7, "="]);
     expect(runningTotal.textContent).toEqual("3");
   });
 
   it("should concatenate multiple number buttons", () => {
-    chainClicks([button1, button1, button5]);
+    chainClicks([1, 1, 5]);
     expect(runningTotal.textContent).toEqual("115");
   });
 
   it("should chain multiple operations", () => {
-    chainClicks([
-      button3,
-      buttonPlus,
-      button4,
-      buttonMultiply,
-      button2,
-      buttonEquals,
-    ]);
+    chainClicks([3, "+", 4, "*", 2, "="]);
     expect(runningTotal.textContent).toEqual("14");
   });
 
   it("should clear the running total without affecting the calculation", () => {
-    chainClicks([
-      button2,
-      buttonPlus,
-      button4,
-      buttonPlus,
-      button5,
-      buttonClear,
-      button1,
-      buttonEquals,
-    ]);
+    chainClicks([2, "+", 4, "+", 5, "C", 1, "="]);
     expect(runningTotal.textContent).toEqual("7");
   });
 });
