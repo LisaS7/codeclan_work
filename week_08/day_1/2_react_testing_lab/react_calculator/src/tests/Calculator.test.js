e25a061222ab47e7707dc57b1e90414ed2ae6f2d
Lisa <mclisa00@gmail.com>
2023-03-13 14:29:41 +0000

    Refactored tests

--- week_08/day_1/2_react_testing_lab/react_calculator/src/tests/Calculator.test.js
+++ week_08/day_1/2_react_testing_lab/react_calculator/src/tests/Calculator.test.js
@@ -4,23 +4,27 @@ import { render, fireEvent } from "@testing-library/react";
 
 describe("Calculator", () => {
   let container;
+  let button1, button4, button7;
+  let buttonPlus, buttonMinus, buttonEquals;
+  let runningTotal;
+
   beforeEach(() => {
     container = render(<Calculator />);
+    button1 = container.getByTestId("number1");
+    button4 = container.getByTestId("number4");
+    button7 = container.getByTestId("number7");
+    buttonPlus = container.getByTestId("operator-add");
+    buttonMinus = container.getByTestId("operator-subtract");
+    buttonEquals = container.getByTestId("operator-equals");
+    runningTotal = container.getByTestId("running-total");
   });
 
   it("should change running total on number enter", () => {
-    const button4 = container.getByTestId("number4");
-    const runningTotal = container.getByTestId("running-total");
     fireEvent.click(button4);
     expect(runningTotal.textContent).toEqual("4");
   });
 
   it("should add two numbers", () => {
-    const button1 = container.getByTestId("number1");
-    const button4 = container.getByTestId("number4");
-    const buttonPlus = container.getByTestId("operator-add");
-    const buttonEquals = container.getByTestId("operator-equals");
-    const runningTotal = container.getByTestId("running-total");
     fireEvent.click(button1);
     fireEvent.click(buttonPlus);
     fireEvent.click(button4);
@@ -29,11 +33,6 @@ describe("Calculator", () => {
   });
 
   it("should subtract one number from another", () => {
-    const button4 = container.getByTestId("number4");
-    const button7 = container.getByTestId("number7");
-    const buttonMinus = container.getByTestId("operator-subtract");
-    const buttonEquals = container.getByTestId("operator-equals");
-    const runningTotal = container.getByTestId("running-total");
     fireEvent.click(button7);
     fireEvent.click(buttonMinus);
     fireEvent.click(button4);
