1008c67a6c779a9158ef5bd7673c8d8450b58b2a
Lisa <mclisa00@gmail.com>
2023-03-13 14:53:26 +0000

    Update display test passing

--- week_08/day_1/2_react_testing_lab/react_calculator/cypress/e2e/calculator.cy.js
+++ week_08/day_1/2_react_testing_lab/react_calculator/cypress/e2e/calculator.cy.js
@@ -1,10 +1,21 @@
 describe("Calculator", () => {
+  function clickButtons(buttons) {
+    for (let button of buttons) {
+      cy.get(button).click();
+    }
+  }
+
   beforeEach(() => {
     cy.visit("http://localhost:3000");
-  })
+  });
 
-  it('should have working number buttons', () => {
-    cy.get('#number2').click();
-    cy.get('.display').should('contain', '2')
-  })
-})
+  it("should have working number buttons", () => {
+    cy.get("#number2").click();
+    cy.get(".display").should("contain", "2");
+  });
+
+  it("should update the running total display", () => {
+    clickButtons(["#number3", "#number1", "#number5"]);
+    cy.get(".display").should("contain", "315");
+  });
+});
