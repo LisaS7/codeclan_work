b4615e537af91430ef0ce99523995ba86486c30b
Lisa <mclisa00@gmail.com>
2023-03-13 12:02:07 +0000

    Added some css

--- week_08/day_1/react_testing/src/components/Counter.js
+++ week_08/day_1/react_testing/src/components/Counter.js
@@ -16,17 +16,23 @@ const Counter = () => {
       <h1 data-testid="counter" id="counter">
         {counter}
       </h1>
-      <button data-testid="button-up" id="button-up" onClick={incrementCounter}>
-        {" "}
-        Up
-      </button>
-      <button
-        data-testid="button-down"
-        id="button-down"
-        onClick={decrementCounter}
-      >
-        Down
-      </button>
+      <div>
+        <button
+          data-testid="button-up"
+          id="button-up"
+          onClick={incrementCounter}
+        >
+          {" "}
+          Up
+        </button>
+        <button
+          data-testid="button-down"
+          id="button-down"
+          onClick={decrementCounter}
+        >
+          Down
+        </button>
+      </div>
     </>
   );
 };
