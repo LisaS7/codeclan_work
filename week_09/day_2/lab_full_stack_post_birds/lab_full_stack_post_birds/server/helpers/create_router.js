ad053bdcd43fc9d2ce499a81766a40334616ed76
Lisa <mclisa00@gmail.com>
2023-03-21 15:54:49 +0000

    MVP done. No idea how to do edit for front end

--- week_09/day_2/lab_full_stack_post_birds/lab_full_stack_post_birds/server/helpers/create_router.js
+++ week_09/day_2/lab_full_stack_post_birds/lab_full_stack_post_birds/server/helpers/create_router.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const ObjectID = require('mongodb').ObjectID;
+const express = require("express");
+const ObjectID = require("mongodb").ObjectID;
 
 const createRouter = function (collection) {
-
   const router = express.Router();
 
-  router.get('/', (req, res) => {
+  router.get("/", (req, res) => {
     collection
       .find()
       .toArray()
@@ -17,11 +16,46 @@ const createRouter = function (collection) {
       });
   });
 
-  router.get('/:id', (req, res) => {
+  router.get("/:id", (req, res) => {
     const id = req.params.id;
     collection
       .findOne({ _id: ObjectID(id) })
       .then((doc) => res.json(doc))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  router.post("/", (req, res) => {
+    collection
+      .insertOne(req.body)
+      .then((result) => res.json(result.ops[0]))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    collection
+      .deleteOne({ _id: ObjectID(id) })
+      .then((doc) => res.json(doc))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    collection
+      .updateOne({ _id: ObjectID(id) }, { $set: req.body })
+      .then((result) => res.json(result))
       .catch((err) => {
         console.error(err);
         res.status(500);
