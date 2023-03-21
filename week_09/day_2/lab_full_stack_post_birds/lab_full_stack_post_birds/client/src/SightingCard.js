1a65e66b623c1bca03a831a54812fa872eae5353
Lisa <mclisa00@gmail.com>
2023-03-21 16:28:39 +0000

    Can't get edit to work

--- week_09/day_2/lab_full_stack_post_birds/lab_full_stack_post_birds/client/src/SightingCard.js
+++ week_09/day_2/lab_full_stack_post_birds/lab_full_stack_post_birds/client/src/SightingCard.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { deleteSighting, putSighting } from "./SightingService";
-import SightingsEditForm from "./SightingsEditForm";
 
 const SightingCard = ({ sighting, removeSighting, editSighting }) => {
   const [edit, setEdit] = useState(false);
+  const [species, setSpecies] = useState(sighting.species);
+  const [location, setLocation] = useState(sighting.location);
   console.log(sighting);
   const handleDelete = () => {
     deleteSighting(sighting._id).then(() => {
@@ -13,19 +14,59 @@ const SightingCard = ({ sighting, removeSighting, editSighting }) => {
 
   const handleUpdate = () => {
     setEdit(!edit);
-    putSighting(sighting._id).then(() => {
-      editSighting(sighting._id);
-    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    sighting[e.target.id] = 0;
+    console.log("change", e.target.value);
   };
 
   return (
     <>
-      <h1>{sighting.species}</h1>
-      <p>Location: {sighting.location}</p>
-      <p>Date: {sighting.date}</p>
+      {edit ? (
+        <form onSubmit={onSubmit} id="sightings-form">
+          <label htmlFor="species">Species:</label>
+          <input
+            type="text"
+            id="species"
+            name="species"
+            value={sighting.species}
+            onChange={onChange}
+          />
+        </form>
+      ) : (
+        <h1>{sighting.species}</h1>
+      )}
+      {edit ? (
+        <form onSubmit={onSubmit} id="sightings-form">
+          <label htmlFor="location">Location:</label>
+          <input type="text" value={sighting.location} onChange={onChange} />
+        </form>
+      ) : (
+        <p>Location: {sighting.location}</p>
+      )}
+
+      {edit ? (
+        <form onSubmit={onSubmit} id="sightings-form">
+          <label htmlFor="date">Date:</label>
+          <input
+            type="date"
+            id="date"
+            name="date"
+            value={sighting.date}
+            onChange={onChange}
+          />
+        </form>
+      ) : (
+        <p>Date: {sighting.date}</p>
+      )}
+
       <button onClick={handleUpdate}> ✏️ </button>
       <button onClick={handleDelete}> 🗑 </button>
-      {edit && <SightingsEditForm sighting={sighting} />}
       <hr></hr>
     </>
   );
