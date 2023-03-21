d63152ac2ce96d47703106d5ddb4dbbf99524a40
Lisa <mclisa00@gmail.com>
2023-03-21 12:00:06 +0000

    Added cors

--- week_09/day_2/mongodb_with_server/mongodb_with_server_games/server/server.js
+++ week_09/day_2/mongodb_with_server/mongodb_with_server_games/server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 const createRouter = require("./helpers/create_router");
 
+app.use(cors());
 app.use(express.json());
 
 MongoClient.connect("mongodb://127.0.0.1:27017", { useUnifiedTopology: true })
