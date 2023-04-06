ec1c6afb8445621c720a99bbdfcc71de86bda579
Lisa <mclisa00@gmail.com>
2023-04-06 13:27:51 +0100

    Reverse words challenge complete

--- week_11/day_04/technical_testing/coding-challenges/reverse-words/codewars.py
+++ week_11/day_04/technical_testing/coding-challenges/reverse-words/codewars.py
@@ -1,5 +1,13 @@
 def spin_words(sentence):
-  pass 
+    sentence_list = sentence.split(" ")
+    new_sentence = []
+
+    for word in sentence_list:
+        if len(word) >= 5:
+            new_sentence.append(word[::-1])
+        else:
+            new_sentence.append(word)
+    return " ".join(new_sentence)
 
 
-assert spin_words('Hello my name is Timothy') == 'olleH my name is yhtomiT'
+assert spin_words("Hello my name is Timothy") == "olleH my name is yhtomiT"
