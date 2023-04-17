52bc795ca6bc42593dfadf26c0cd81b0923afdbc
Lisa <mclisa00@gmail.com>
2023-04-17 15:38:10 +0100

    Starting homework

---
+++ week_13/day_1/homework/ThemeParkStartPoint/src/test/java/attractions/PlaygroundTest.java
@@ -0,0 +1,43 @@
+package attractions;
+
+import org.junit.Before;
+import org.junit.Test;
+import people.Visitor;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlaygroundTest {
+    Playground playground;
+
+    @Before
+    public void setUp() throws Exception {
+        playground = new Playground("Fun Zone", 7);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Fun Zone", playground.getName());
+    }
+
+    @Test
+    public void hasRating() {
+        assertEquals(7, playground.getRating());
+    }
+
+    @Test
+    public void hasVisitCount() {
+        assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void hasMaxAgeRestriction__false() {
+        Visitor visitor = new Visitor(23, 1.8, 10.50);
+        assertEquals(false, playground.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void hasMaxAgeRestriction__true() {
+        Visitor visitor = new Visitor(10, 1.2, 4.30);
+        assertEquals(true, playground.isAllowedTo(visitor));
+    }
+}
